b6faa55a270035d99070fce67a9064afbd5b7e88
Quinn Battagliese <qbattagliese@Quinns-MBP.cshl.edu>
2024-07-16 16:25:14 -0400

    first commit

---
+++ Nodes.py
@@ -0,0 +1,182 @@
+import numpy as np
+import random
+
+#RootNode Class
+#Initializes a node with a pointer to 1 child
+class RootNode:
+  def __init__(self, number = 1000):
+    self.number = number
+    self.child = None
+    self.parent = None
+
+  def __str__(self):
+    return "root"
+
+#Node Class
+#Functions include __init__() - intializes object, height() - determines height
+#of node, full()- determines if a specific node is full, add_node(new_node) -
+#adds a new node to the node it is called on, traverse_GAP(GAP) - given a GAP,
+#traverses from that node,
+class Node:
+  def __init__(self, number = 0):
+    self.left = None
+    self.right = None
+    self.parent = None
+    self.number = number
+
+
+  def height(self):
+    #calculates the height of a given node
+    if isinstance(self, LeafNode):
+      return 0
+    elif isinstance(self, Node):
+      if self.left is not None and self.right is not None:
+        return max(1 + self.left.height(), 1 + self.right.height())
+      elif self.left is not None:
+        return 1 + self.left.height()
+      elif self.right is not None:
+        return 1 + self.right.height()
+      else:
+        return 0
+    else:
+      p=0
+
+
+  def full(self):
+    #checks to see if the tree is full
+    if isinstance(self, LeafNode):
+      return True
+    elif self.left is not None and self.right is not None:
+      return True
+    elif self.left is not None and self.right is not None:
+      if self.left.height() != self.right.height():
+        return False
+      else:
+        return self.left.full() and self.right.full()
+    else:
+      return False #node with one child is not full
+
+
+  # def add_node_in_order(self, new_node):
+  #   #given a current node and a new node will add the new node in the first
+  #   #available position to maintain the fullness of the tree
+  #   if isinstance(self.left, LeafNode, self.right, LeafNode):
+  #     return
+  #   if self.left is None:
+  #     self.left = new_node
+  #     new_node.parent = self
+  #     self.leaf = False
+  #   elif self.right is None:
+  #     self.right = new_node
+  #     new_node.parent = self
+  #     self.leaf = False
+  #   else:
+  #     if self.left.full() is True and self.right.full() is True:
+  #       if self.left.height() > self.right.height():
+  #         self.right.add_node_in_order(new_node)
+  #       else:
+  #         self.left.add_node_in_order(new_node)
+  #     elif self.left.full() is False:
+  #       self.left.add_node_in_order(new_node)
+  #     else:
+  #       self.right.add_node_in_order(new_node)
+
+
+  def traverse_GAP(self, gap):
+    #Recursive function to traverse tree until a leaf is reached. When a leaf
+    #is reached the probability associated with that node is returned
+    # if self.leaf is True:
+    #   # print(f'''The predicted probability of the mouse
+    #   # licking based on {gap} is: {self.probability}''')
+    #   return self.probability
+    if isinstance(self, LeafNode):
+      return self.prob
+    if gap[self.number] == 0: # or gap[self.number] == "0":
+      return self.left.traverse_GAP(gap)
+    elif gap[self.number] == 1: # or gap[self.number] =="1":
+      return self.right.traverse_GAP(gap)
+    else:
+      raise ValueError("Invalid glomerulus state")
+
+  def traverse_and_update(self, gap, response):
+    # print("traversing")
+    #traverses tree with a gap/sucess combo and updates leaf probabilities
+    if isinstance(self, LeafNode):
+      self.update_probability(response)
+      return
+    # for some reason, this is reading the gap as a string
+    if gap[self.number] == 0 or gap[self.number] == "0":
+      self.left.traverse_and_update(gap, response)
+    elif self.right is not None:
+      self.right.traverse_and_update(gap, response)
+
+  def add_node(self, location, new_node):
+    #Manually adds a new node at a given location to the current node 
+    #Location = 0: left, Location = 1: right.
+    if location == 0:
+      self.left = new_node
+      new_node.parent = self
+    else:
+      self.right = new_node
+      new_node.parent = self
+  
+  def create_temp_node(self):
+    #creates a temporary node with the same pointers as the given node
+    temp_node = Node(number = self.number)
+    temp_node.parent = self.parent
+    temp_node.left = self.left
+    temp_node.right = self.right
+    return temp_node
+
+  def depth(self):
+    #returns the depth of a node within a tree
+    if self.parent is None:
+      return 0
+    else:
+      return 1 + self.parent.depth()
+    
+  def __str__(self):
+    return f"Node {self.number}"
+  
+
+class LeafNode(Node):
+  def __init__(self, init_prob = 0):
+    self.parent = None
+    self.licks = 0
+    self.total_trials = 0
+    self.prob = init_prob
+    # if self.total_trials:
+    #     self.number = self.licks/self.total_trials
+
+  def update_probability(self, response):
+    #Based on the success or failure (1 or 0) of a trail, a leafs probability will be updated
+    if response == 1:
+      self.licks += 1
+    self.total_trials += 1
+    self.prob = self.licks/self.total_trials
+ 
+  def __str__(self):
+    return "leaf"
+  
+# start_node = Node(0)
+# start_left = Node(1)
+# start_node.left = start_left
+# start_right = Node(2)
+# start_node.right = start_right
+# leaf1 = LeafNode(0.1)
+# start_left.left = leaf1
+# leaf2 = LeafNode(0.2)
+# start_left.right = leaf2
+# leaf3 = LeafNode(0.3)
+# start_right.left = leaf3
+# leaf4 = LeafNode(0.4)
+# start_right.right = leaf4
+# print(start_node.traverse_GAP("000"))
+# print("_____________________")
+# print(start_node.traverse_GAP([0, 0, 0]))
+# print("_____________________")
+# print(start_node.traverse_GAP("110"))
+# print("_____________________")
+# print(start_node.traverse_GAP([1, 1, 0]))
+
+  
---
+++ gaps.py
@@ -0,0 +1,231 @@
+import Tree as t
+import Nodes as n
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import graphviz
+import copy
+from datetime import datetime
+import os
+import ast
+
+TREE_MUTATION_ERROR = {}
+# ERROR_DICT = {}
+# OUTPUT_DICT = {}
+# GAP_OUTPUT = {}
+# TRAIN_ERROR = {}
+
+np.random.seed(1)
+random.seed(1)
+now = datetime.now().strftime("%Y%m%d_%H%M%S")
+results_dir_path = os.path.join("./results", now)
+os.mkdir(results_dir_path)
+
+
+def read_gap(gap):
+  #converts a string into a readable list to traverse a tree
+  new_gap = []
+  for x in range(len(gap)):
+    new_gap.append(int(gap[x]))
+  return new_gap
+
+
+def binary_num_list(x, prefix = ''):
+  #creates all possible gap activation patterns of a certain size
+  if x == 0:
+    return [prefix]
+  else:
+    return (binary_num_list(x - 1, prefix + '0') +
+            binary_num_list(x - 1, prefix + '1'))
+
+def run_GAPS(tree, test_list):
+  #Given a list of gaps, traverses a given tree for all gaps in the list
+  #Fill out empty OUTPUT_DICT, used to calculate error for each iteration
+  #BUG: currently only going to farthest right node
+  response_list = []
+  for gap in test_list:
+    prob = tree.root.child.traverse_GAP(gap)
+    if prob not in tree.OUTPUT_DICT:
+      tree.OUTPUT_DICT[prob] = [0, 0]
+  x = 0
+  for gap in test_list:
+    test_prob = tree.root.child.traverse_GAP(gap)
+    test_val = random.choices([0, 1], weights = (1-test_prob, test_prob))[0]
+    response_list.append(test_val)
+    tree.OUTPUT_DICT[test_prob][0] += test_val
+    tree.OUTPUT_DICT[test_prob][1] += 1
+    if str(gap) not in tree.GAP_OUTPUT:
+      tree.GAP_OUTPUT[str(gap)] = test_val
+    x += 1
+  return response_list
+
+def train_GAPS(tree, train_dict: dict):
+  #given a tree and a dictionary with gaps as keys and successes as values, trains the tree
+  curr_tree = tree
+  for key,val in train_dict.items():
+    gap = ast.literal_eval(key)
+    output = val
+    curr_tree.root.child.traverse_and_update(gap, output)
+
+def assess_error(test_tree_responses, ground_truth_responses):
+  #given a trained tree and ground truth model, error of the trained tree is assessed and returned
+  error = np.abs(sum(ground_truth_responses)- sum(test_tree_responses))
+  # error = 0
+  # for gaps in test_tree.GAP_OUTPUT.keys():
+  #   error += np.abs(ground_truth.GAP_OUTPUT[gaps] - test_tree.GAP_OUTPUT[gaps])
+  return error
+
+def train_tree_with_mutations(start_tree, ground_truth_responses, train_set, validate_set):
+  #Starting with a random tree and a ground truth model, this functions modifies trees until 10 trees are constructed, each of which
+  #performed better than the previous tree
+  # trees_tested = 0
+  successful_tree_trace = []
+  
+  prev_tree = start_tree
+  successful_tree_trace.append(prev_tree)  
+  train_GAPS(prev_tree, train_set)
+  prev_responses = run_GAPS(prev_tree, validate_set)
+  prev_tree_error = assess_error(prev_responses, ground_truth_responses)
+  num_successful_mutations = 0
+  while num_successful_mutations < 5:
+    curr_tree = prev_tree.mutate()
+    train_GAPS(curr_tree, train_set)
+    curr_responses = run_GAPS(curr_tree, validate_set)
+    curr_tree_error = assess_error(curr_responses, ground_truth_responses)
+    # print(f"prev tree error = {prev_tree_error}")
+    # print(f"curr tree error = {curr_tree_error}")
+    if curr_tree_error < prev_tree_error: 
+      prev_tree = copy.deepcopy(curr_tree)
+      prev_tree_error = curr_tree_error
+      successful_tree_trace.append(prev_tree)
+      num_successful_mutations += 1
+      TREE_MUTATION_ERROR[num_successful_mutations] = curr_tree_error
+  return prev_tree
+   
+    
+
+#after making a list of all possible gap patterns, randomly selects 500 test
+#gaps and prints them out
+binary_list = binary_num_list(10)
+test_list = []
+for x in range(0,1000):
+  test_list.append(read_gap(random.choice(binary_list)))
+# print(test_list)
+
+
+#adds leafs to the decision tree
+leaf_list = []
+leaf6 = n.LeafNode(0.55)
+leaf_list.append(leaf6)
+leaf7 = n.LeafNode(0.65)
+leaf_list.append(leaf7)
+leaf8 = n.LeafNode(0.75)
+leaf_list.append(leaf8)
+leaf9 = n.LeafNode(0.85)
+leaf_list.append(leaf9)
+leaf10 = n.LeafNode(0.95)
+leaf_list.append(leaf10)
+leaf1 = n.LeafNode(0.05)
+leaf_list.append(leaf1)
+leaf = n.LeafNode(0.10)
+leaf_list.append(leaf)
+leaf2 = n.LeafNode(0.15)
+leaf_list.append(leaf2)
+leaf3 = n.LeafNode(0.25)
+leaf_list.append(leaf3)
+leaf4 = n.LeafNode(0.35)
+leaf_list.append(leaf4)
+leaf5 = n.LeafNode(0.45)
+leaf_list.append(leaf5)
+
+
+#Create, fill, and print tree
+print("ground truth: ")
+ground_truth = t.build_bald_tree(10, shuffle_nums=False)
+ground_truth = t.fill_tree_with_leaves(ground_truth, evenly_spaced=True)
+ground_truth.print2D(ground_truth.root.child)
+print("__________________________________________")
+
+ground_truth_results = run_GAPS(ground_truth, test_list)
+
+test_tree = t.build_bald_tree(10, shuffle_nums=True)
+test_tree = t.fill_tree_with_leaves(test_tree, evenly_spaced=False)
+test_tree.print2D(test_tree.root.child)
+print("__________________________________________")
+
+
+train_gap_keys = random.sample(list(ground_truth.GAP_OUTPUT.keys()), 500)
+train_gap_dict = {key: ground_truth.GAP_OUTPUT[key] for key in train_gap_keys}
+# for key,val in train_gap_dict.items():
+#   print(f"key: {type(key)}, val: {val}")
+
+# train_GAPS(test_tree, train_gaps)
+# test_tree.print2D(test_tree.root.child)
+
+validate_set = {}
+while len(validate_set) <= 100:
+  new_gap = random.choice(list(ground_truth.GAP_OUTPUT.keys()))
+  if new_gap not in train_gap_dict and new_gap not in validate_set:
+    validate_set[new_gap] = ground_truth.GAP_OUTPUT[new_gap]
+
+validate_list = []
+for key,val in validate_set.items():
+    gap = ast.literal_eval(key)
+    validate_list.append(gap)
+
+
+final_tree = train_tree_with_mutations(test_tree, ground_truth_results, train_gap_dict, validate_list)
+final_tree.print2D(final_tree.root.child)
+
+for key,val in TREE_MUTATION_ERROR.items():
+  print(f"Mutation: {key} | Error: {val}")
+
+p = 0
+# train_GAPS(test_tree, train_gaps)
+# test_tree.print2D(test_tree.root.child)
+
+# run_GAPS(test_tree, validate_set)
+# print(test_tree.OUTPUT_DICT)
+
+
+# test_tree = train_tree_with_mutations(test_tree, ground_truth, train_gaps, validate_list)
+
+# run_GAPS(test_tree,validate_list)
+# print(assess_error(test_tree, tree1))
+
+# test_tree = test_tree.mutate()
+# test_tree.print2D(test_tree.root.child)
+
+
+# x = np.array(list(results.keys()))
+# y = np.array([v[0] / v[1] for v in results.values()])
+# fig, ax = plt.subplots()
+# ax.scatter(x, y)
+# plt.xlabel('p(lick)')
+# plt.ylabel('p̂(lick)')
+# b, a = np.polyfit(x, y, deg=1)
+# plt.plot(x, a + b * x)
+# plt.title('Simulated Prob vs. True Prob')
+# # plt.show()
+
+
+# #Print and graph model error
+# x = np.array(list(tree1.ERROR_DICT.keys())) 
+# y = np.array(list(tree1.ERROR_DICT.values()))
+# fig, ax = plt.subplots()
+# ax.scatter(x, y)
+# plt.xlabel('iteration')
+# plt.ylabel('error')
+# b, a = np.polyfit(x, y, deg=1)
+# plt.title('Error vs. Iteration')
+# # plt.savefig(os.path.join(results_dir_path, "error_vs_itr.png"))
+# plt.show()
+
+
+# ##create tree to train: 
+# train_tree = t.Tree(0, 10)
+# train_tree.create_tree_random(8)
+# for x in range(9):
+#   new_leaf = n.LeafNode()
+#   train_tree.root.child.add_node_in_order(new_leaf)
+# t.print2D(train_tree.root.child)
